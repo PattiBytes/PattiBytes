703ef693206a3f6c6fb42189229ae40fa22ecfc5
PattiBytes <pattibytesltdbusiness@gmail.com>
2025-09-22 22:29:24 +0530

    Update firebase-config.js

--- app/assets/js/firebase-config.js
+++ app/assets/js/firebase-config.js
@@ -1,11 +1,12 @@
-<script>
-  window.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDSp4mE15rFn1Tdo4YARZ4MD3_vdnsYyog",
-    authDomain: "pattibytes-cf602.firebaseapp.com",
-    projectId: "pattibytes-cf602",
-    storageBucket: "pattibytes-cf602.appspot.com",
-    messagingSenderId: "980629232960",
-    appId: "1:980629232960:web:c169821e1f4bb409c2aea5",
-    measurementId: "G-WGMH00XGJC"
-  };
-</script>
+// Firebase configuration (plain script, no imports)
+// Required for all auth flows: email/password, Google, and reset password.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDSp4mE15rFn1Tdo4YARZ4MD3_vdnsYyog",
+  authDomain: "pattibytes-cf602.firebaseapp.com",
+  projectId: "pattibytes-cf602",
+  // New default domain format is valid for recent Firebase projects.
+  storageBucket: "pattibytes-cf602.firebasestorage.app",
+  messagingSenderId: "980629232960",
+  appId: "1:980629232960:web:c169821e1f4bb409c2aea5",
+  measurementId: "G-WGMH00XGJC"
+};
