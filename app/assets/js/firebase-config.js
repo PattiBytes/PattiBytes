02b69495e1cedb8efbb880a733f8dd70706a30d9
PattiBytes <pattibytesltdbusiness@gmail.com>
2025-09-22 22:15:12 +0530

    Update firebase-config.js

--- app/assets/js/firebase-config.js
+++ app/assets/js/firebase-config.js
@@ -1,4 +1,4 @@
-
+<script>
   window.FIREBASE_CONFIG = {
     apiKey: "AIzaSyDSp4mE15rFn1Tdo4YARZ4MD3_vdnsYyog",
     authDomain: "pattibytes-cf602.firebaseapp.com",
@@ -8,4 +8,4 @@
     appId: "1:980629232960:web:c169821e1f4bb409c2aea5",
     measurementId: "G-WGMH00XGJC"
   };
-
+</script>
